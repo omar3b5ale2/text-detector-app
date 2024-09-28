2eeb1a7f9cbe98aab951a034c18e5969d80d8646
OM AR <om1446587@gmail.com>
2024-09-28 22:37:56 +0300

    3b5aleq

---
+++ android/app/src/main/kotlin/com/example/textdetection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.textdetection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/splash/view/pages/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:textdetection/core/constant/app_constants.dart';
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue, // Replace with your desired color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/png/Detective Logo.png'), // Replace with your logo image path
+            const SizedBox(height: 20),
+            const Text(
+              AppConstants.appName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
